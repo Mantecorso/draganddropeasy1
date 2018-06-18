1eb48216455eff28360dfd5776e6f3283e5fc345
Mantecorso <luisjuradoquesada@gmail.com>
2018-06-18 19:06:06 +0200

    Funcionagit add .

---
+++ main.js
@@ -0,0 +1,31 @@
+//function allowDrop(ev) {
+//    ev.preventDefault();
+//  }
+   
+//  function ondragstart(ev) {
+//    ev.dataTransfer.setData("text", ev.target.id);
+//  }
+   
+//  function ondrop(ev) {
+//    ev.preventDefault();
+//    var data = ev.dataTransfer.getData("text");
+//    ev.target.appendChild(document.getElementById(data));
+//  }
+  
+
+  
+function onDrop(ev){
+  console.log('ondrop')
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function onDragStart(ev) {
+
+  ev.dataTransfer.setData("text", ev.target.id); // guardamos los datos de la imagen en el evento que mas tarde vamos a pasar a onDrop (el ID en este caso)
+}
